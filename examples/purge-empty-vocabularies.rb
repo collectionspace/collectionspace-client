f7e167b1f973e002817dc9f5071bd4a06f07af6d
Mark Cooper <markchristophercooper@gmail.com>
2016-01-04 15:48:21 -0800

    Add purge empty vocabularies example

---
+++ examples/purge-empty-vocabularies.rb
@@ -0,0 +1,16 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'awesome_print'
+require 'collectionspace/client'
+
+# CREATE CLIENT WITH DEFAULT (DEMO) CONFIGURATION -- BE NICE!!!
+client = CollectionSpace::Client.new
+client.config.throttle = 1
+
+client.all('vocabularies') do |item|
+  uri = item["uri"]
+  if client.count("#{uri}/items") == 0
+    puts "Purging empty vocabulary:\t#{item['displayName']} (#{item['csid']})"
+    # YOU WOULD UNCOMMENT THIS TO ACTUALLY PERFORM THE PURGE ...
+    # client.delete uri
+  end
+end
