fa63d603a967a2a779e3109c53fb1c63cd7edb94
Mark Cooper <mark.c.cooper@outlook.com>
2020-06-18 16:30:09 -0700

    Support additional params for search

--- spec/collectionspace/search_spec.rb
+++ spec/collectionspace/search_spec.rb
@@ -1,4 +1,31 @@
 require 'spec_helper'
 
 describe CollectionSpace::Search do
+  let(:search_args) {{
+    path: 'groups',
+    type: 'groups_common',
+    field: 'title',
+    expression: "ILIKE '%D%'"
+  }}
+  it 'can construct search from a hash' do
+    search = CollectionSpace::Search.new(search_args)
+    expect(search.path).to eq(search_args[:path])
+    expect(search.type).to eq(search_args[:type])
+    expect(search.field).to eq(search_args[:field])
+    expect(search.expression).to eq(search_args[:expression])
+  end
+
+  it 'can add params for search' do
+    client = CollectionSpace::Client.new(CollectionSpace::Configuration.new)
+    allow(client).to receive(:request).and_return nil
+    client.search(
+      CollectionSpace::Search.new(search_args),
+      { sortBy: 'collectionspace_core:updatedAt DESC' }
+    )
+    expect(client).to have_received(:request).with(
+      'GET', 'groups', {
+        query: {as: "groups_common:title ILIKE '%D%'", sortBy: 'collectionspace_core:updatedAt DESC'}
+      }
+    )
+  end
 end
