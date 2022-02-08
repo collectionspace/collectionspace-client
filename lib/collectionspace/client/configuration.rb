946e592b6945c13908034f1168fb6d795c50345d
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-02-07 19:00:59 -0500

    remove class attr creation from instance method

--- lib/collectionspace/client/configuration.rb
+++ lib/collectionspace/client/configuration.rb
@@ -15,14 +15,34 @@ module CollectionSpace
       }
     end
 
+    attr_accessor :foo
     def initialize(settings = {})
       settings = defaults.merge(settings)
       settings.each do |property, value|
-        next unless defaults.key? property
+        next unless defaults.key?(property)
 
         instance_variable_set("@#{property}", value)
-        self.class.send(:attr_accessor, property)
+        #self.class.send(:attr_accessor, property)
       end
+
+      attributes = defaults.keys
+      set_class_attrs(attributes) unless class_attrs_exist?(attributes)
+    end
+    
+    private
+
+    def class_attrs_exist?(settings)
+      settings.all?{ |setting| methods.any?(setting) }
+    end
+
+    def set_class_attr(setting)
+      return if [setting, "#{setting}=".to_sym].all?{ |meth| methods.any?(meth) }
+
+      self.class.send(:attr_accessor, setting)
+    end
+    
+    def set_class_attrs(settings)
+      settings.each{ |setting| set_class_attr(setting) }
     end
   end
 end
