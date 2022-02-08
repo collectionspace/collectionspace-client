3311b094b7cbd64d3d5f89e473d8a97181b294a9
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-02-07 20:05:33 -0500

    simplify setting `attr_accessors` by doing it manually
    The previous metaprogramming experiment was fun and educational
    but this will be easier to make sense of and maintain. Though
    some static code analyzer is sure to eventually complain this
    class has too many attr_accessors or methods...

--- lib/collectionspace/client/configuration.rb
+++ lib/collectionspace/client/configuration.rb
@@ -3,39 +3,25 @@
 module CollectionSpace
   # CollectionSpace configuration
   class Configuration
-    class << self
-      def defaults
-        {
-          base_uri: nil,
-          username: nil,
-          password: nil,
-          page_size: 25,
-          include_deleted: false,
-          throttle: 0,
-          verify_ssl: true
-        }
-      end
+    DEFAULTS = {
+      base_uri: nil,
+      username: nil,
+      password: nil,
+      page_size: 25,
+      include_deleted: false,
+      throttle: 0,
+      verify_ssl: true
+    }.freeze
 
-      # The first time a Configuration instance is created, creates `attr_accessor` for each key
-      #   in defaults
-      # Then it undefines and redefines itself as a blank method.
-      # The `undef` is required to avoid 'method redefined' warnings
-      def set_attr_accessors
-        defaults.keys.each{ |setting| send(:attr_accessor, setting) }
-        undef :set_attr_accessors
-        define_singleton_method(:set_attr_accessors){}
-      end
-    end
+    attr_accessor :base_uri, :username, :password, :page_size, :include_deleted, :throttle, :verify_ssl
 
     def initialize(settings = {})
-      settings = self.class.defaults.merge(settings)
+      settings = DEFAULTS.merge(settings)
       settings.each do |property, value|
-        next unless self.class.defaults.key?(property)
+        next unless DEFAULTS.key?(property)
 
         instance_variable_set("@#{property}", value)
       end
-
-      self.class.set_attr_accessors
     end
   end
 end
