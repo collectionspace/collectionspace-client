48a95542beba3728165f262c2aa77695150d89c8
Mark Cooper <mark.c.cooper@outlook.com>
2022-07-21 09:32:03 -0700

    Add VCR cassette for all live specs

--- spec/collectionspace/helpers_spec.rb
+++ spec/collectionspace/helpers_spec.rb
@@ -60,7 +60,9 @@ describe CollectionSpace::Helpers do
     context "with object" do
       let(:args) { {type: "collectionobjects", value: "QA TEST 001"} }
       it "finds as expected" do
-        expect(result).to eq("/collectionobjects/56c04f5f-32b9-4f1d-8a4b")
+        VCR.use_cassette("helpers_find_with_collectionobject") do
+          expect(result).to eq("/collectionobjects/56c04f5f-32b9-4f1d-8a4b")
+        end
       end
     end
 
@@ -77,20 +79,22 @@ describe CollectionSpace::Helpers do
           {type: "orgauthorities", subtype: "organization", value: "Smith's Appletree Garager"},
           {type: "orgauthorities", subtype: "organization", value: 'The "Grand" Canyon'}
         ]
-        results = args.map { |arg| client.find(arg) }
-          .map { |resp| resp.parsed["abstract_common_list"]["list_item"]["uri"] }
-        expected = [
-          "/placeauthorities/838dbc1c-12f0-45fa-9a26/items/40adef7a-aadc-4743-b2ed",
-          "/placeauthorities/838dbc1c-12f0-45fa-9a26/items/e4f1148d-1790-417c-ab7a",
-          "/placeauthorities/838dbc1c-12f0-45fa-9a26/items/c9d34920-1782-49ed-a0c3",
-          "/placeauthorities/838dbc1c-12f0-45fa-9a26/items/4745b7b1-cc7a-458e-9742",
-          "/placeauthorities/838dbc1c-12f0-45fa-9a26/items/a0f4ba2a-07cb-4647-a884",
-          "/personauthorities/0f6cddfa-32ce-4c25-9b2f/items/2f050460-984b-49d7-b6df",
-          "/personauthorities/0f6cddfa-32ce-4c25-9b2f/items/699abb7c-9a14-48cc-a975",
-          "/orgauthorities/5225cf0b-d288-41ab-b2ea/items/02ed1508-9a29-451e-a08b",
-          "/orgauthorities/5225cf0b-d288-41ab-b2ea/items/bf51a88c-2eae-48d6-9405"
-        ]
-        expect(results).to eq(expected)
+        VCR.use_cassette("helpers_find_with_authority_term") do
+          results = args.map { |arg| client.find(arg) }
+            .map { |resp| resp.parsed["abstract_common_list"]["list_item"]["uri"] }
+          expected = [
+            "/placeauthorities/838dbc1c-12f0-45fa-9a26/items/40adef7a-aadc-4743-b2ed",
+            "/placeauthorities/838dbc1c-12f0-45fa-9a26/items/e4f1148d-1790-417c-ab7a",
+            "/placeauthorities/838dbc1c-12f0-45fa-9a26/items/c9d34920-1782-49ed-a0c3",
+            "/placeauthorities/838dbc1c-12f0-45fa-9a26/items/4745b7b1-cc7a-458e-9742",
+            "/placeauthorities/838dbc1c-12f0-45fa-9a26/items/a0f4ba2a-07cb-4647-a884",
+            "/personauthorities/0f6cddfa-32ce-4c25-9b2f/items/2f050460-984b-49d7-b6df",
+            "/personauthorities/0f6cddfa-32ce-4c25-9b2f/items/699abb7c-9a14-48cc-a975",
+            "/orgauthorities/5225cf0b-d288-41ab-b2ea/items/02ed1508-9a29-451e-a08b",
+            "/orgauthorities/5225cf0b-d288-41ab-b2ea/items/bf51a88c-2eae-48d6-9405"
+          ]
+          expect(results).to eq(expected)
+        end
       end
     end
 
@@ -100,7 +104,9 @@ describe CollectionSpace::Helpers do
         {type: "vocabularies", subtype: "annotationtype", value: "Additional Taxa", operator: "ILIKE"}
       end
       it "finds as expected" do
-        expect(result).to eq("/vocabularies/e1401111-05c2-4d6c-bdc5/items/84c82c13-9d46-48a9-a8b9")
+        VCR.use_cassette("helpers_find_with_ilike") do
+          expect(result).to eq("/vocabularies/e1401111-05c2-4d6c-bdc5/items/84c82c13-9d46-48a9-a8b9")
+        end
       end
     end
 
@@ -110,7 +116,9 @@ describe CollectionSpace::Helpers do
         {type: "vocabularies", subtype: "annotationtype", value: "additional %", operator: "LIKE"}
       end
       it "finds as expected" do
-        expect(result).to eq("/vocabularies/e1401111-05c2-4d6c-bdc5/items/84c82c13-9d46-48a9-a8b9")
+        VCR.use_cassette("helpers_find_with_like") do
+          expect(result).to eq("/vocabularies/e1401111-05c2-4d6c-bdc5/items/84c82c13-9d46-48a9-a8b9")
+        end
       end
     end
   end
@@ -122,7 +130,9 @@ describe CollectionSpace::Helpers do
     context "with object hierarchy" do
       let(:args) { {subject_csid: "16161bff-b01a-4b55-95aa", object_csid: "34bb1c08-5f46-4347-94db", rel_type: "hasBroader"} }
       it "finds as expected" do
-        expect(result).to eq("/relations/e23631b8-a977-46b8-b4b9")
+        VCR.use_cassette("helpers_find_object_hierarchy") do
+          expect(result).to eq("/relations/e23631b8-a977-46b8-b4b9")
+        end
       end
     end
 
@@ -130,7 +140,9 @@ describe CollectionSpace::Helpers do
       let(:args) { {subject_csid: "e4f1148d-1790-417c-ab7a", object_csid: "40adef7a-aadc-4743-b2ed", rel_type: "hasBroader"} }
 
       it "finds as expected" do
-        expect(result).to eq("/relations/1a35c85f-a549-48ec-bfc3")
+        VCR.use_cassette("helpers_find_authority_hierarchy") do
+          expect(result).to eq("/relations/1a35c85f-a549-48ec-bfc3")
+        end
       end
     end
 
@@ -138,7 +150,9 @@ describe CollectionSpace::Helpers do
       let(:args) { {subject_csid: "56c04f5f-32b9-4f1d-8a4b", object_csid: "6f0ce7b3-0130-444d-8633", rel_type: "affects"} }
 
       it "finds as expected" do
-        expect(result).to eq("/relations/53b4a988-cd8a-4299-9ae7")
+        VCR.use_cassette("helpers_find_non_hierarchical_relation") do
+          expect(result).to eq("/relations/53b4a988-cd8a-4299-9ae7")
+        end
       end
     end
 
@@ -146,13 +160,17 @@ describe CollectionSpace::Helpers do
       let(:args) { {subject_csid: "56c04f5f-32b9-4f1d-8a4b", object_csid: "6f0ce7b3-0130-444d-8633"} }
 
       it "finds as expected" do
-        expect(result).to eq("/relations/53b4a988-cd8a-4299-9ae7")
+        VCR.use_cassette("helpers_find_with_no_reltype") do
+          expect(result).to eq("/relations/53b4a988-cd8a-4299-9ae7")
+        end
       end
 
       it "warns" do
         msg = "No rel_type specified, so multiple types of relations between 56c04f5f-32b9-4f1d-8a4b and 6f0ce7b3-0130-444d-8633 may be returned"
-        expect(client).to receive(:warn).with(msg, uplevel: 1)
-        response
+        VCR.use_cassette("helpers_find_with_no_reltype") do
+          expect(client).to receive(:warn).with(msg, uplevel: 1)
+          response
+        end
       end
     end
   end
@@ -174,36 +192,38 @@ describe CollectionSpace::Helpers do
         {type: "personauthorities", subtype: "person", value: "Linda Colet"}
 
       ]
-      results = args.map { |arg| client.keyword_search(arg) }
-        .map { |response| response.parsed["abstract_common_list"]["list_item"] }
-        .map { |list| list.is_a?(Hash) ? [list] : list } # handle single item returned
-        .map { |list| list.nil? ? [{}] : list } # handle no items returned
-        .map { |list| list.map { |item| item["uri"] } }
-        .flatten
+      VCR.use_cassette("helpers_find_keyword_search") do
+        results = args.map { |arg| client.keyword_search(arg) }
+          .map { |response| response.parsed["abstract_common_list"]["list_item"] }
+          .map { |list| list.is_a?(Hash) ? [list] : list } # handle single item returned
+          .map { |list| list.nil? ? [{}] : list } # handle no items returned
+          .map { |list| list.map { |item| item["uri"] } }
+          .flatten
 
-      expected = [
-        "/vocabularies/e1401111-05c2-4d6c-bdc5/items/84c82c13-9d46-48a9-a8b9",
-        "/vocabularies/e1401111-05c2-4d6c-bdc5/items/84c82c13-9d46-48a9-a8b9",
-        "/collectionobjects/ac04b8a6-db59-433e-872f",
-        "/collectionobjects/8b21c9af-1fab-4708-91d4",
-        "/collectionobjects/16e51d6a-5ae3-4716-bd0f",
-        "/collectionobjects/bf51110a-0666-47b8-b9d4",
-        "/collectionobjects/77c07515-c0e3-4b76-aeea",
-        "/collectionobjects/bf51110a-0666-47b8-b9d4",
-        "/collectionobjects/bf51110a-0666-47b8-b9d4",
-        nil,
-        "/personauthorities/0f6cddfa-32ce-4c25-9b2f/items/f7464b3c-f2a9-4c7a-bf5d",
-        "/personauthorities/0f6cddfa-32ce-4c25-9b2f/items/2661dcf8-f184-41db-b032",
-        "/personauthorities/0f6cddfa-32ce-4c25-9b2f/items/2c4e4938-482d-4574-946b",
-        "/personauthorities/0f6cddfa-32ce-4c25-9b2f/items/67235e6f-5fc1-4319-b4df",
-        "/personauthorities/0f6cddfa-32ce-4c25-9b2f/items/e4b4c37c-9243-4eb4-807b",
-        "/personauthorities/0f6cddfa-32ce-4c25-9b2f/items/e0e83104-85bc-48ba-acef",
-        "/personauthorities/0f6cddfa-32ce-4c25-9b2f/items/67235e6f-5fc1-4319-b4df",
-        "/personauthorities/0f6cddfa-32ce-4c25-9b2f/items/7b110f01-acac-4742-bdf0",
-        "/personauthorities/0f6cddfa-32ce-4c25-9b2f/items/2fd23671-b476-4f67-b548",
-        "/personauthorities/0f6cddfa-32ce-4c25-9b2f/items/67235e6f-5fc1-4319-b4df"
-      ]
-      expect(results).to eq(expected)
+        expected = [
+          "/vocabularies/e1401111-05c2-4d6c-bdc5/items/84c82c13-9d46-48a9-a8b9",
+          "/vocabularies/e1401111-05c2-4d6c-bdc5/items/84c82c13-9d46-48a9-a8b9",
+          "/collectionobjects/8b21c9af-1fab-4708-91d4",
+          "/collectionobjects/ac04b8a6-db59-433e-872f",
+          "/collectionobjects/16e51d6a-5ae3-4716-bd0f",
+          "/collectionobjects/bf51110a-0666-47b8-b9d4",
+          "/collectionobjects/77c07515-c0e3-4b76-aeea",
+          "/collectionobjects/bf51110a-0666-47b8-b9d4",
+          "/collectionobjects/bf51110a-0666-47b8-b9d4",
+          nil,
+          "/personauthorities/0f6cddfa-32ce-4c25-9b2f/items/f7464b3c-f2a9-4c7a-bf5d",
+          "/personauthorities/0f6cddfa-32ce-4c25-9b2f/items/2661dcf8-f184-41db-b032",
+          "/personauthorities/0f6cddfa-32ce-4c25-9b2f/items/2c4e4938-482d-4574-946b",
+          "/personauthorities/0f6cddfa-32ce-4c25-9b2f/items/67235e6f-5fc1-4319-b4df",
+          "/personauthorities/0f6cddfa-32ce-4c25-9b2f/items/e4b4c37c-9243-4eb4-807b",
+          "/personauthorities/0f6cddfa-32ce-4c25-9b2f/items/e0e83104-85bc-48ba-acef",
+          "/personauthorities/0f6cddfa-32ce-4c25-9b2f/items/67235e6f-5fc1-4319-b4df",
+          "/personauthorities/0f6cddfa-32ce-4c25-9b2f/items/7b110f01-acac-4742-bdf0",
+          "/personauthorities/0f6cddfa-32ce-4c25-9b2f/items/2fd23671-b476-4f67-b548",
+          "/personauthorities/0f6cddfa-32ce-4c25-9b2f/items/67235e6f-5fc1-4319-b4df"
+        ]
+        expect(results).to eq(expected)
+      end
     end
   end
 
@@ -213,7 +233,9 @@ describe CollectionSpace::Helpers do
       let(:id) { "DTS.1" }
       let(:url) { "not_a_url" }
       it "will report an argument error" do
-        expect { client.reset_media_blob(id, url) }.to raise_error(CollectionSpace::ArgumentError)
+        VCR.use_cassette("helpers_reset_media_blob_invalid_url") do
+          expect { client.reset_media_blob(id, url) }.to raise_error(CollectionSpace::ArgumentError)
+        end
       end
     end
 
@@ -221,7 +243,9 @@ describe CollectionSpace::Helpers do
       let(:id) { "DTS.does_not_exist" }
       let(:url) { "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" }
       it "will report a not found error" do
-        expect { client.reset_media_blob(id, url) }.to raise_error(CollectionSpace::NotFoundError)
+        VCR.use_cassette("helpers_reset_media_blob_does_not_exist") do
+          expect { client.reset_media_blob(id, url) }.to raise_error(CollectionSpace::NotFoundError)
+        end
       end
     end
 
@@ -229,7 +253,9 @@ describe CollectionSpace::Helpers do
       let(:id) { "DTS.dupes" }
       let(:url) { "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" }
       it "will report a duplicate id error" do
-        expect { client.reset_media_blob(id, url) }.to raise_error(CollectionSpace::DuplicateIdFound)
+        VCR.use_cassette("helpers_reset_media_blob_duplicate") do
+          expect { client.reset_media_blob(id, url) }.to raise_error(CollectionSpace::DuplicateIdFound)
+        end
       end
     end
 
@@ -237,11 +263,13 @@ describe CollectionSpace::Helpers do
       let(:id) { "DTS.1" }
       let(:url) { "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" }
       it "will reset the media blob record" do
-        response = client.find(type: "media", value: id, field: "identificationNumber")
-        blob_csid = response.parsed["abstract_common_list"]["list_item"]["blobCsid"]
-        response = client.reset_media_blob(id, url)
-        expect(response.result.success?).to be true
-        expect(response.parsed["document"]["media_common"]["blobCsid"]).to_not eq blob_csid
+        VCR.use_cassette("helpers_reset_media_blob_success") do
+          response = client.find(type: "media", value: id, field: "identificationNumber")
+          blob_csid = response.parsed["abstract_common_list"]["list_item"]["blobCsid"]
+          response = client.reset_media_blob(id, url)
+          expect(response.result.success?).to be true
+          expect(response.parsed["document"]["media_common"]["blobCsid"]).to_not eq blob_csid
+        end
       end
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,7 @@ RSpec.configure do |config|
 end
 
 VCR.configure do |c|
-  c.allow_http_connections_when_no_cassette = true
+  c.allow_http_connections_when_no_cassette = false
   c.cassette_library_dir = "spec/fixtures/cassettes"
   c.hook_into :webmock
   c.default_cassette_options = {record: :once}
