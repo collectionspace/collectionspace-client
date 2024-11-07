ff5840a9721370cf503dcabf79d442724e25e42f
Mark Cooper <mark.c.cooper@outlook.com>
2024-11-07 12:27:49 -0800

    Allow fulltext reindex to work across everything
    This provides an option for triggering a pg fulltext reindex using
    the api in a fire and forget manner.

--- spec/collectionspace/template_spec.rb
+++ spec/collectionspace/template_spec.rb
@@ -18,17 +18,41 @@ describe CollectionSpace::Template do
     ENV.delete("COLLECTIONSPACE_CLIENT_TEMPLATES_PATH")
   end
 
-  it "can process a template" do
-    xml = Nokogiri::XML.parse(CollectionSpace::Template.process(:reindex_full_text, {}))
-    expect(xml.xpath("//name").text).to eq "Reindex Full Text"
+  context "Report template" do
+    it "can generate payload xml" do
+      data = {name: "A brilliant report!", doctype: "CollectionObject", filename: "report.pdf"}
+      xml = Nokogiri::XML.parse(CollectionSpace::Template.process(:report, data))
 
-    xml = Nokogiri::XML.parse(
-      CollectionSpace::Template.process(
-        :reindex_by_doctype, {
-          doctype: "CollectionObject"
-        }
-      )
-    )
-    expect(xml.xpath("//docType").text).to eq "CollectionObject"
+      expect(xml.xpath("//name").text).to eq data[:name]
+      expect(xml.xpath("//forDocType").text).to eq data[:doctype]
+      expect(xml.xpath("//filename").text).to eq data[:filename]
+    end
+  end
+
+  context "Reindex template" do
+    it "can generate reindex all payload xml" do
+      data = {doctype: nil, csids: []}
+      xml = Nokogiri::XML.parse(CollectionSpace::Template.process(:reindex, data))
+
+      expect(xml.xpath("//mode").text).to eq "nocontext"
+      expect(xml.xpath("//docType").text).to be_empty
+    end
+
+    it "can generate reindex by doctype payload xml" do
+      data = {doctype: "CollectionObject", csids: []}
+      xml = Nokogiri::XML.parse(CollectionSpace::Template.process(:reindex, data))
+
+      expect(xml.xpath("//mode").text).to eq "nocontext"
+      expect(xml.xpath("//docType").text).to eq "CollectionObject"
+    end
+
+    it "can generate reindex by doctype and csids payload xml" do
+      data = {doctype: "CollectionObject", csids: ["123", "456"]}
+      xml = Nokogiri::XML.parse(CollectionSpace::Template.process(:reindex, data))
+
+      expect(xml.xpath("//mode").text).to eq "list"
+      expect(xml.xpath("//docType").text).to eq "CollectionObject"
+      expect(xml.xpath("//csid").text).to eq data[:csids].join("")
+    end
   end
 end
